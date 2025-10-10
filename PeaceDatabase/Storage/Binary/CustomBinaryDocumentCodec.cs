9060d23c6c0b83155d65bb1fb5bbc6d98e4f49ba
timermakov <ts.ermakov@yandex.ru>
2025-10-10 04:38:27 +0300

    Updated to use Little Endian, UTF-8 without Byte Order Mark (BOM), with exceptions

--- PeaceDatabase/Storage/Binary/CustomBinaryDocumentCodec.cs
+++ PeaceDatabase/Storage/Binary/CustomBinaryDocumentCodec.cs
@@ -1,5 +1,6 @@
 ﻿// File: Storage/Binary/CustomBinaryDocumentCodec.cs
 using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -9,6 +10,8 @@ namespace PeaceDatabase.Storage.Binary
 {
     /// <summary>
     /// Собственный бинарный формат сериализации Document (PeaceDBDocument v1, порядок записи Type-Length-Value).
+    /// Все числовые значения пишутся в little-endian вне зависимости от архитектуры.
+    /// Строки кодируются строгим UTF-8 (без BOM), при некорректных байтах генерируется исключение.
     /// </summary>
     public static class CustomBinaryDocumentCodec
     {
@@ -22,98 +25,100 @@ namespace PeaceDatabase.Storage.Binary
         private const byte ContentField = 7;
         private const byte FormatVersion = 1;
 
+        // Строгий UTF-8: без BOM, выбрасывает исключение на некорректных последовательностях
+        private static readonly Encoding Utf8NoBomStrict = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+
         public static byte[] Serialize(Document doc)
         {
             using var ms = new MemoryStream();
-            using var bw = new BinaryWriter(ms, Encoding.UTF8, leaveOpen: true);
 
-            // Version
-            bw.Write(VersionField);     // type
-            bw.Write(1);                // length
-            bw.Write(FormatVersion);    // value
+            // Version (TLV)
+            WriteByte(ms, VersionField);     // type
+            WriteInt32LE(ms, 1);             // length
+            WriteByte(ms, FormatVersion);    // value
 
             // Id
             if (!string.IsNullOrEmpty(doc.Id))
-                WriteStringTLV(bw, IdField, doc.Id);
+                WriteStringTLV(ms, IdField, doc.Id);
             // Rev
             if (!string.IsNullOrEmpty(doc.Rev))
-                WriteStringTLV(bw, RevField, doc.Rev);
+                WriteStringTLV(ms, RevField, doc.Rev);
             // Deleted
-            bw.Write(DeletedField);
-            bw.Write(1);
-            bw.Write(doc.Deleted ? (byte)1 : (byte)0);
+            WriteByte(ms, DeletedField);
+            WriteInt32LE(ms, 1);
+            WriteByte(ms, doc.Deleted ? (byte)1 : (byte)0);
             // Data
-            if (doc.Data != null && doc.Data.Count > 0)
+            if (doc.Data is { Count: > 0 })
             {
                 var dataBytes = SerializeData(doc.Data);
-                bw.Write(DataField);
-                bw.Write(dataBytes.Length);
-                bw.Write(dataBytes);
+                WriteByte(ms, DataField);
+                WriteInt32LE(ms, dataBytes.Length);
+                ms.Write(dataBytes, 0, dataBytes.Length);
             }
             // Tags
-            if (doc.Tags != null && doc.Tags.Count > 0)
+            if (doc.Tags is { Count: > 0 })
             {
                 using var tagsMs = new MemoryStream();
-                using var tagsBw = new BinaryWriter(tagsMs, Encoding.UTF8, leaveOpen: true);
-                tagsBw.Write(doc.Tags.Count);
+                WriteInt32LE(tagsMs, doc.Tags.Count);
                 foreach (var tag in doc.Tags)
-                    WriteString(tagsBw, tag);
-                tagsBw.Flush();
+                    WriteString(tagsMs, tag);
                 var tagsBytes = tagsMs.ToArray();
-                bw.Write(TagsField);
-                bw.Write(tagsBytes.Length);
-                bw.Write(tagsBytes);
+                WriteByte(ms, TagsField);
+                WriteInt32LE(ms, tagsBytes.Length);
+                ms.Write(tagsBytes, 0, tagsBytes.Length);
             }
             // Content
             if (!string.IsNullOrEmpty(doc.Content))
-                WriteStringTLV(bw, ContentField, doc.Content);
+                WriteStringTLV(ms, ContentField, doc.Content);
 
-            bw.Flush();
             return ms.ToArray();
         }
 
         public static Document Deserialize(byte[] data)
         {
-            using var ms = new MemoryStream(data);
-            using var br = new BinaryReader(ms, Encoding.UTF8, leaveOpen: true);
+            using var ms = new MemoryStream(data, writable: false);
             var doc = new Document();
             while (ms.Position < ms.Length)
             {
-                var fieldId = br.ReadByte();
-                var len = br.ReadInt32();
+                var fieldId = ReadByte(ms);
+                var len = ReadInt32LE(ms);
                 var fieldStart = ms.Position;
                 switch (fieldId)
                 {
                     case VersionField:
-                        var ver = br.ReadByte();
-                        if (ver != FormatVersion)
-                            throw new NotSupportedException($"Unsupported format version: {ver}");
-                        break;
+                        {
+                            var ver = ReadByte(ms);
+                            if (ver != FormatVersion)
+                                throw new NotSupportedException($"Unsupported format version: {ver}");
+                            break;
+                        }
                     case IdField:
-                        doc.Id = ReadString(br, len);
+                        doc.Id = ReadString(ms, len);
                         break;
                     case RevField:
-                        doc.Rev = ReadString(br, len);
+                        doc.Rev = ReadString(ms, len);
                         break;
                     case DeletedField:
-                        doc.Deleted = br.ReadByte() != 0;
+                        doc.Deleted = ReadByte(ms) != 0;
                         break;
                     case DataField:
-                        doc.Data = DeserializeData(br.ReadBytes(len));
-                        break;
-                    case TagsField:
-                        using (var tagsMs = new MemoryStream(br.ReadBytes(len)))
-                        using (var tagsBr = new BinaryReader(tagsMs, Encoding.UTF8))
                         {
-                            var count = tagsBr.ReadInt32();
+                            var payload = ReadExact(ms, len);
+                            doc.Data = DeserializeData(payload);
+                            break;
+                        }
+                    case TagsField:
+                        {
+                            using var tagsMs = new MemoryStream(ReadExact(ms, len), writable: false);
+                            var count = ReadInt32LE(tagsMs);
                             var tags = new List<string>(count);
                             for (int i = 0; i < count; i++)
-                                tags.Add(ReadString(tagsBr));
+                                tags.Add(ReadString(tagsMs));
                             doc.Tags = tags;
+                            break;
                         }
-                        break;
                     case ContentField:
-                        doc.Content = ReadString(br, len);
+                        doc.Content = ReadString(ms, len);
                         break;
                     default:
                         // skip unknown field
@@ -125,29 +130,94 @@ namespace PeaceDatabase.Storage.Binary
         }
 
         // --- Helpers ---
-        private static void WriteStringTLV(BinaryWriter bw, byte fieldId, string value)
+        private static void WriteStringTLV(Stream s, byte fieldId, string value)
         {
-            var bytes = Encoding.UTF8.GetBytes(value);
-            bw.Write(fieldId);
-            bw.Write(bytes.Length);
-            bw.Write(bytes);
+            var bytes = Utf8NoBomStrict.GetBytes(value);
+            WriteByte(s, fieldId);
+            WriteInt32LE(s, bytes.Length);
+            s.Write(bytes, 0, bytes.Length);
         }
-        private static void WriteString(BinaryWriter bw, string value)
+        private static void WriteString(Stream s, string value)
         {
-            var bytes = Encoding.UTF8.GetBytes(value);
-            bw.Write(bytes.Length);
-            bw.Write(bytes);
+            var bytes = Utf8NoBomStrict.GetBytes(value);
+            WriteInt32LE(s, bytes.Length);
+            s.Write(bytes, 0, bytes.Length);
         }
-        private static string ReadString(BinaryReader br, int len)
+        private static string ReadString(Stream s, int len)
         {
-            var bytes = br.ReadBytes(len);
-            return Encoding.UTF8.GetString(bytes);
+            var bytes = ReadExact(s, len);
+            return Utf8NoBomStrict.GetString(bytes);
         }
-        private static string ReadString(BinaryReader br)
+        private static string ReadString(Stream s)
         {
-            var len = br.ReadInt32();
-            var bytes = br.ReadBytes(len);
-            return Encoding.UTF8.GetString(bytes);
+            var len = ReadInt32LE(s);
+            var bytes = ReadExact(s, len);
+            return Utf8NoBomStrict.GetString(bytes);
+        }
+
+        private static void WriteByte(Stream s, byte b) => s.WriteByte(b);
+        private static byte ReadByte(Stream s)
+        {
+            int b = s.ReadByte();
+            if (b < 0) throw new EndOfStreamException();
+            return (byte)b;
+        }
+        private static void WriteInt32LE(Stream s, int value)
+        {
+            Span<byte> buf = stackalloc byte[4];
+            BinaryPrimitives.WriteInt32LittleEndian(buf, value);
+            s.Write(buf);
+        }
+        private static int ReadInt32LE(Stream s)
+        {
+            Span<byte> buf = stackalloc byte[4];
+            ReadExact(s, buf);
+            return BinaryPrimitives.ReadInt32LittleEndian(buf);
+        }
+        private static void WriteInt64LE(Stream s, long value)
+        {
+            Span<byte> buf = stackalloc byte[8];
+            BinaryPrimitives.WriteInt64LittleEndian(buf, value);
+            s.Write(buf);
+        }
+        private static long ReadInt64LE(Stream s)
+        {
+            Span<byte> buf = stackalloc byte[8];
+            ReadExact(s, buf);
+            return BinaryPrimitives.ReadInt64LittleEndian(buf);
+        }
+        private static void WriteDoubleLE(Stream s, double value)
+        {
+            long bits = BitConverter.DoubleToInt64Bits(value);
+            WriteInt64LE(s, bits);
+        }
+        private static double ReadDoubleLE(Stream s)
+        {
+            long bits = ReadInt64LE(s);
+            return BitConverter.Int64BitsToDouble(bits);
+        }
+        private static byte[] ReadExact(Stream s, int len)
+        {
+            if (len < 0) throw new IOException("Negative length");
+            var buffer = new byte[len];
+            int read = 0;
+            while (read < len)
+            {
+                int r = s.Read(buffer, read, len - read);
+                if (r <= 0) throw new EndOfStreamException();
+                read += r;
+            }
+            return buffer;
+        }
+        private static void ReadExact(Stream s, Span<byte> buffer)
+        {
+            int readTotal = 0;
+            while (readTotal < buffer.Length)
+            {
+                int r = s.Read(buffer.Slice(readTotal));
+                if (r <= 0) throw new EndOfStreamException();
+                readTotal += r;
+            }
         }
 
         // --- Data (Dictionary<string, object>) ---
@@ -165,44 +235,39 @@ namespace PeaceDatabase.Storage.Binary
         private static byte[] SerializeData(Dictionary<string, object> data)
         {
             using var ms = new MemoryStream();
-            using var bw = new BinaryWriter(ms, Encoding.UTF8, leaveOpen: true);
-            bw.Write(data.Count);
+            WriteInt32LE(ms, data.Count);
             foreach (var kv in data)
             {
-                WriteString(bw, kv.Key);
-                WriteDataValue(bw, kv.Value);
+                WriteString(ms, kv.Key);
+                WriteDataValue(ms, kv.Value);
             }
-            bw.Flush();
             return ms.ToArray();
         }
         private static Dictionary<string, object> DeserializeData(byte[] bytes)
         {
             var dict = new Dictionary<string, object>();
-            using var ms = new MemoryStream(bytes);
-            using var br = new BinaryReader(ms, Encoding.UTF8);
-            var count = br.ReadInt32();
+            using var ms = new MemoryStream(bytes, writable: false);
+            var count = ReadInt32LE(ms);
             for (int i = 0; i < count; i++)
             {
-                var key = ReadString(br);
-                var type = (DataType)br.ReadByte();
+                var key = ReadString(ms);
+                var type = (DataType)ReadByte(ms);
                 object? value = type switch
                 {
                     DataType.Null => null,
-                    DataType.String => ReadString(br),
-                    DataType.Int => br.ReadInt32(),
-                    DataType.Double => br.ReadDouble(),
-                    DataType.Bool => br.ReadByte() != 0,
-                    DataType.ListString =>
-                        ReadStringList(br),
-                    DataType.Dict =>
-                        DeserializeData(br.ReadBytes(br.ReadInt32())),
+                    DataType.String => ReadString(ms),
+                    DataType.Int => ReadInt32LE(ms),
+                    DataType.Double => ReadDoubleLE(ms),
+                    DataType.Bool => ReadByte(ms) != 0,
+                    DataType.ListString => ReadStringList(ms),
+                    DataType.Dict => DeserializeData(ReadNested(ms)),
                     _ => throw new NotSupportedException($"Unknown DataType: {type}")
                 };
                 dict[key] = value!;
             }
             return dict;
         }
-        private static void WriteDataValue(BinaryWriter bw, object? value)
+        private static void WriteDataValue(Stream s, object? value)
         {
             // Новая нормализация JsonElement -> CLR
             if (value is System.Text.Json.JsonElement je)
@@ -211,45 +276,49 @@ namespace PeaceDatabase.Storage.Binary
             switch (value)
             {
                 case null:
-                    bw.Write((byte)DataType.Null);
+                    WriteByte(s, (byte)DataType.Null);
                     break;
-                case string s:
-                    bw.Write((byte)DataType.String);
-                    WriteString(bw, s);
+                case string str:
+                    WriteByte(s, (byte)DataType.String);
+                    WriteString(s, str);
                     break;
-                case int i:
-                    bw.Write((byte)DataType.Int);
-                    bw.Write(i);
+                case int i32:
+                    WriteByte(s, (byte)DataType.Int);
+                    WriteInt32LE(s, i32);
                     break;
-                case long l:
-                    bw.Write((byte)DataType.Int);
-                    bw.Write(checked((int)l));
+                case long i64:
+                    WriteByte(s, (byte)DataType.Int);
+                    WriteInt32LE(s, checked((int)i64));
                     break;
                 case double d:
-                    bw.Write((byte)DataType.Double);
-                    bw.Write(d);
+                    WriteByte(s, (byte)DataType.Double);
+                    WriteDoubleLE(s, d);
                     break;
                 case float f:
-                    bw.Write((byte)DataType.Double);
-                    bw.Write((double)f);
+                    WriteByte(s, (byte)DataType.Double);
+                    WriteDoubleLE(s, (double)f);
                     break;
                 case bool b:
-                    bw.Write((byte)DataType.Bool);
-                    bw.Write(b ? (byte)1 : (byte)0);
+                    WriteByte(s, (byte)DataType.Bool);
+                    WriteByte(s, b ? (byte)1 : (byte)0);
                     break;
                 case IEnumerable<string> list:
-                    bw.Write((byte)DataType.ListString);
-                    var arr = new List<string>(list);
-                    bw.Write(arr.Count);
-                    foreach (var s in arr)
-                        WriteString(bw, s);
-                    break;
+                    {
+                        WriteByte(s, (byte)DataType.ListString);
+                        var arr = new List<string>(list);
+                        WriteInt32LE(s, arr.Count);
+                        foreach (var item in arr)
+                            WriteString(s, item);
+                        break;
+                    }
                 case Dictionary<string, object> dict:
-                    bw.Write((byte)DataType.Dict);
-                    var dictBytes = SerializeData(dict);
-                    bw.Write(dictBytes.Length);
-                    bw.Write(dictBytes);
-                    break;
+                    {
+                        WriteByte(s, (byte)DataType.Dict);
+                        var dictBytes = SerializeData(dict);
+                        WriteInt32LE(s, dictBytes.Length);
+                        s.Write(dictBytes, 0, dictBytes.Length);
+                        break;
+                    }
                 default:
                     throw new NotSupportedException($"Unsupported data type: {value.GetType().Name}");
             }
@@ -302,13 +371,19 @@ namespace PeaceDatabase.Storage.Binary
             }
         }
 
-        private static List<string> ReadStringList(BinaryReader br)
+        private static List<string> ReadStringList(Stream s)
         {
-            var count = br.ReadInt32();
+            var count = ReadInt32LE(s);
             var list = new List<string>(count);
             for (int i = 0; i < count; i++)
-                list.Add(ReadString(br));
+                list.Add(ReadString(s));
             return list;
+        }
+
+        private static byte[] ReadNested(Stream s)
+        {
+            var nestedLen = ReadInt32LE(s);
+            return ReadExact(s, nestedLen);
         }
     }
 }
